d9059731b77ba56c033ec9d7e2136591c02bd843
Hugo <hugolenoe@gmail.com>
2024-03-25 12:02:07 +0100

    new controller forgot

--- code/controller/loginController.php
+++ code/controller/loginController.php
@@ -8,9 +8,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_POST["username"]) && isset($_POST["password"]) && $_POST["username"] != '' && $_POST["password"] != ''){
         $userTest = new User(null, null, null, $_POST["username"],$_POST["password"], null);
         
+        $_SESSION['username'] = $_POST["username"];
         
-        $_SESSION['connect'] = true;
-
     }
 }
 ?>
