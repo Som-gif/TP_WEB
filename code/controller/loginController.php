fb3b0db98c09f0b1f07ebb5589f0bc76cf1961cb
Hugo <hugolenoe@gmail.com>
2024-03-25 11:26:45 +0100

    start LoginController

---
+++ code/controller/loginController.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+require_once "../model/UsersDAO.php";
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST["username"]) && isset($_POST["password"]) && $_POST["username"] != '' && $_POST["password"] != ''){
+        $userTest = new User(null, null, null, $_POST["username"],$_POST["password"], null);
+        
+        
+        $_SESSION['connect'] = true;
+
+    }
+}
+?>
